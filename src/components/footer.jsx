74135d102158096c78105a9b4c4edcc0192645b4
ifiokLab2 <ifiokudoh6@gmail.com>
2025-02-20 17:23:21 +0100

    hh

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,37 +11,29 @@ const Footer = ()=>{
                     <div class="footer-card">
                         <div class="f-headline f-item">LET US HELP YOU</div>
                         <div class="f-item">
-                            <Link href =''>Contact Us</Link>
+                            <Link to =''>Contact Us</Link>
                         </div>
 
                     </div>
                     <div class="footer-card">
-                        <div class="f-headline f-item">ABOUT DiasporaBlack</div>
+                        <div class="f-headline f-item">ABOUT</div>
                         <div class="f-item">
                             <Link href =''>About Us</Link>
                         </div>
-                        <div class="f-item">
-                            <Link href =''>Contact Us</Link>
-                        </div>
+                       
 
                     </div>
                     <div class="footer-card">
-                        <div class="f-headline f-item">MAKE MONEY BY BECOMING AN INSTRUCTOR</div>
+                        <div class="f-headline f-item">Latest Post</div>
                         <div class="f-item">
-                            <Link href =''>Teach</Link>
+                            <Link href =''>Admisson</Link>
                         </div>
 
 
 
                     </div>
                     <div class="footer-card">
-                        <div class="f-headline ">ELEARNING INTERNATIONAL</div>
-                        <div class="f-item">
-                            <Link href =''>Teach on ELEARNING</Link>
-                        </div>
-                        <div class="f-item">
-                            <a href="">UK</a>
-                        </div>
+                        
 
                     </div>
                 </div>
